db75434bbfd19ead1d94243863047026d6e116dc
Roya <roya.song@obigo.com>
2024-05-30 18:04:16 +0900

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging.js');
  
 // Initialize Firebase
 var config = {
