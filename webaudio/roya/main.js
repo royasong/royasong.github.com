d894de641f9739fce1e3623cc5f46a725dc7dffa
roya song <roya.song@obigo.com>
2025-02-05 08:38:23 +0900

    add

--- webaudio/roya/main.js
+++ webaudio/roya/main.js
@@ -1,36 +1,52 @@
-var audio = document.getElementById("audio-source");
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var source = audioCtx.createMediaElementSource(audio);
-var gainNode = audioCtx.createGain();
+const AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var gainConnected = source.connect(gainNode);
-gainConnected.connect(audioCtx.destination);
+const audioContext = new AudioContext();
 
-function play() {
-    if (audio.paused) {
-        audio.play()
-    }
-    else {
-        audio.pause()
-    }
-}
+//get the audio element
+const audioElement = document.querySelector('audio');
 
-function volumeUp() {
-    
-    if (gainNode.gain.value > 2) {
-        gainNode.gain.value = 2
-    }
-    else {
-        gainNode.gain.value += 0.2
-    }
-}
+//pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
+// track.connect(audioContext.destination);
 
-function volumeDown() {
-    
-    if (gainNode.gain.value < 0) {
-        gainNode.gain.value = 0
+//select our play button
+const playButton = document.querySelector('button');
+
+playButton.addEventListener('click', function (){
+    //check if context is in suspended state (auto policy)
+    if(audioContext.state === 'suspended'){
+        audioContext.resume();
     }
-    else {
-        gainNode.gain.value -= 0.2 
+    console.log("this.dataset : ", this.dataset);
+    //play or pause track depending on state
+    if(this.dataset.playing === 'false'){
+        audioElement.play();
+        this.dataset.playing = 'true'
+    }else if(this.dataset.playing === 'true'){
+        audioElement.pause();
+        this.dataset.playing = 'false';
     }
-}
+},false);
+
+audioElement.addEventListener('ended', () => {
+    playButton.dataset.playing = 'false';
+}, false);
+
+const gainNode = audioContext.createGain();
+// track.connect(gainNode).connect(audioContext.destination);
+
+const volumeControl = document.querySelector('#volume');
+
+volumeControl.addEventListener('input', function(){
+    gainNode.gain.value = this.value
+}, false);
+
+const pannerOptions = { pan : 0 };
+const panner = new StereoPannerNode(audioContext, pannerOptions);
+
+const pannerControl = document.querySelector('#panner');
+
+pannerControl.addEventListener('input', function(){
+    panner.pan.value = this.value;
+}, false);
+track.connect(gainNode).connect(panner).connect(audioContext.destination);
