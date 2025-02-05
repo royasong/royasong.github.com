2ab225edf52bd2b932aef9b1f4e13bbb57cb734f
roya song <roya.song@obigo.com>
2025-02-05 09:20:38 +0900

    test

--- webaudio/roya/main.js
+++ webaudio/roya/main.js
@@ -40,6 +40,21 @@ const volumeControl = document.querySelector('#volume');
 volumeControl.addEventListener('input', function(){
     gainNode.gain.value = this.value
     console.log("(volumeControl) gainNode.gain.value = this.value" + this.value);
+	gainNode.gain.setValueAtTime(2.0, audioContext.currentTime);
+	// gainNode.gain.value = 2.0;
+	track.connect(gainNode);
+
+	let analyserNode = audioContext.createAnalyser();
+	analyserNode.fftSize = 2048;
+	gainNode.connect(analyserNode);
+
+	let pannerOptions = {
+		pan: 0
+	};
+	let pannerNode = new StereoPannerNode(audioContext, pannerOptions);
+	pannerNode.pan.value = 0.5; // range: -1 ~ 1
+	gainNode.connect(pannerNode);
+
 }, false);
 
 const pannerOptions = { pan : 0 };
@@ -48,7 +63,18 @@ const panner = new StereoPannerNode(audioContext, pannerOptions);
 const pannerControl = document.querySelector('#panner');
 
 pannerControl.addEventListener('input', function(){
-    panner.pan.value = this.value;
+    gainNode.gain.value = this.value
+    console.log("(volumeControl) gainNode.gain.value = this.value" + this.value);
+	let test_feedForward =   [0.1215955842, 0.2431911684, 0.1215955842];
+	let test_feedBackward =  [1.2912769759, -1.5136176632, 0.7087230241];
+	const iirfilter = new IIRFilterNode(audioContext, {
+	  feedforward: test_feedForward,
+	  feedback: test_feedBackward,
+	});
+	connect(iirfilter).audioContext(audioCtx.destination);
+
+
+
     console.log("(pannerControl) panner.pan.value = this.value = this.value" + this.value);
 }, false);
 track.connect(gainNode).connect(panner).connect(audioContext.destination);
